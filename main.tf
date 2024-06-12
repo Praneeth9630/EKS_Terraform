8ae1dc7628740d615318c42583f047eaa1dbc1b4
V. Surya Praneeth <159445971+Praneeth9630@users.noreply.github.com>
2024-06-12 13:22:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+  region = "us-east-1" # Specify your desired region
+}
 
 data "aws_subnet" "existing_subnet" {
   id = "subnet-06ac653833db86d12" # Replace with the ID of your existing subnet
@@ -12,7 +15,7 @@ module "eks" {
   version = "18.30.3"
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.24"
-  subnet_ids      = [data.aws_subnet.existing_subnet.id] # Use the existing subnet
+  subnets         = [data.aws_subnet.existing_subnet.id] # Use the existing subnet
   vpc_id          = "vpc-0db38383340f9b93c" # Replace with your VPC ID
   # Other EKS configurations here
 }
@@ -20,14 +23,18 @@ module "eks" {
 resource "aws_eks_cluster" "cluster" {
   name     = module.eks.cluster_name
   role_arn = module.eks.cluster_iam_role_arn
+  vpc_config {
+    subnet_ids = [data.aws_subnet.existing_subnet.id] # Use the existing subnet
+    security_group_ids = [data.aws_security_group.existing_security_group.id]
+  }
   # Other EKS cluster configurations here
 }
 
 resource "aws_eks_node_group" "example" {
   cluster_name    = aws_eks_cluster.cluster.name
   node_group_name = "Node-cloud"
-  node_role_arn    = aws_iam_role.example1.arn
-  subnet_ids       = [data.aws_subnet.existing_subnet.id] # Use the existing subnet
+  node_role_arn   = aws_iam_role.example.arn
+  subnet_ids      = [data.aws_subnet.existing_subnet.id] # Use the existing subnet
 
   scaling_config {
     desired_size = 1
@@ -37,16 +44,22 @@ resource "aws_eks_node_group" "example" {
   instance_types = ["t2.medium"]
 }
 
-resource "aws_iam_role" "example1" {
+resource "aws_iam_role" "example" {
   name = "eks-node-group-example"
-  # Other IAM role configurations here
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
-
-# Use the existing security group for the EKS cluster
-resource "aws_eks_cluster" "example" {
-  # Other EKS cluster configurations here
-  vpc_config {
-    security_group_ids = [data.aws_security_group.existing_security_group.id]
-    # Other VPC configurations here
-  }
+EOF
+  # Other IAM role configurations here
 }
